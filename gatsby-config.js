68dca5160e8f5309a1315477df4203465212e430
Marchel-Ace <marchel.ace@gmail.com>
2021-07-18 21:19:44 +0700

    update manifest and icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,15 +37,15 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it. replace with yours
-        trackingId: "UA-164743872-1",
+        trackingId: "",
         head: true,
       }
     },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Delog GatbsyJS Starter`,
-        short_name: `Delog`,
+        name: `A-Blog`,
+        short_name: `A-Blog`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#381696`,
